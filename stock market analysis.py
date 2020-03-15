c7242cd6be8007a7f274b7da6ff81657b02c1a81
Stijn <stijnservaes19@gmail.com>
2020-03-15 17:00:22 +0100

    Finished part 2 of Stock Market Analysis

--- stock market analysis.py
+++ stock market analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 from pandas import Series, DataFrame
 from pandas_datareader import DataReader
@@ -25,3 +26,30 @@ AAPL.info()
 AAPL['Adj Close'].plot(legend = True, figsize= (20,4))
 
 AAPL['Volume'].plot(legend=True, figsize=(20,4))
+
+ma_day = [10, 20, 50]
+
+for ma in ma_day:
+    column_name = "Moving Average for %s days" %(str(ma))
+    AAPL[column_name] = pd.Series(AAPL["Adj Close"]).rolling(ma).mean()
+
+AAPL[["Adj Close", "Moving Average for 10 days", "Moving Average for 20 days", "Moving Average for 50 days" ]].plot(legend = True, figsize = (20,4))
+
+AAPL["Daily Return"] = AAPL["Adj Close"].pct_change()
+
+AAPL["Daily Return"].plot(figsize = (20,4), legend = True, linestyle = "--", marker = "o")
+
+fig = plt.figure(figsize = (20,4))
+sns.distplot(AAPL["Daily Return"].dropna(), bins = 100, color = 'blue')
+AAPL["Daily Return"].hist(bins = 100, figsize = (20,4))
+
+closing_df = DataReader(tech_list, 'yahoo', start, end)['Adj Close']
+closing_df.head()
+
+tech_rets = closing_df.pct_change()
+tech_rets.head()
+
+sns.jointplot('GOOG', 'GOOG', tech_rets, kind = 'scatter', color = 'seagreen')
+
+fig = sns.jointplot('GOOG', 'AMZN', tech_rets, kind = 'scatter', color = 'blue')
+fig.annotate(stats.pearsonr)
